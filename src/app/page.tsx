dda1d7749a5d7177ae1e90553d633471724037c9
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-11-21 07:34:31 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+
+import Hero from "./components/Hero";
+import Teachers from "./components/Teachers";
+export default function Home() {
+  return (
+   <div>
+    
+    <Hero />
+    <Teachers />
+    <Footer />
+  
+  
+   </div>
+  );
+}
